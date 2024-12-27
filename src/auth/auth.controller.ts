7645f258760af545e67608b1c69ca4a21f370965
terary <terary@gmail.com>
2024-12-27 17:55:13 -0600

    added logout functionality stub

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
+import type { SignInDto, SignOutDto } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
@@ -17,8 +18,15 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.username, signInDto.password);
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Post('logout')
+  signOut(@Body() signOutDto: SignOutDto) {
+    return this.authService.signOut(signOutDto);
   }
 
   @UseGuards(AuthGuard)
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 
+export type SignOutDto = {
+  access_token: string;
+};
+export type SignInDto = {
+  username: string;
+  password: string;
+};
+
 @Injectable()
 export class AuthService {
   constructor(
@@ -9,17 +17,21 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(
-    username: string,
-    pass: string,
-  ): Promise<{ access_token: string }> {
+  async signIn({
+    username,
+    password,
+  }: SignInDto): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(username);
-    if (user?.password !== pass) {
+    if (user?.password !== password) {
       throw new UnauthorizedException();
     }
     const payload = { sub: user.userId, username: user.username };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  async signOut({ access_token }: SignOutDto): Promise<boolean> {
+    return Promise.resolve(true);
   }
 }
